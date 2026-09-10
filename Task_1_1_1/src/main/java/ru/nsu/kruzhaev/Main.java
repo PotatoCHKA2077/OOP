b2a24801bae3ebdd6bff7151411260f3d4fb17b1
Ivan Kruzhaev <i.kruzhaev@g.nsu.ru>
2026-09-10 18:51:28 +0700

    task done

--- Task_1_1_1/src/main/java/ru/nsu/kruzhaev/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/kruzhaev/Main.java
@@ -1,17 +1,87 @@
  package ru.nsu.kruzhaev;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Arrays;
+import java.util.Random;
+
+ //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static int sum(int a, int b){
-        return a+b;
+    public static class Heap {
+        private static int[] buffer;
+        public static int size;
+
+        public Heap(int[] arr){
+            size = 0;
+            buffer = new int[arr.length];
+            for (int elem : arr) {
+                insert(elem);
+            }
+        }
+
+        public void shiftUp(int index) {
+                while (index > 0) {
+                    int curr_idx = index;
+                    int parent_idx = (index - 1) / 2;
+                    if (buffer[curr_idx] < buffer[parent_idx]) {
+                        int t = buffer[curr_idx];
+                        buffer[curr_idx] = buffer[parent_idx];
+                        buffer[parent_idx] = t;
+                        index = parent_idx;
+                    } else {
+                        break;
+                    }
+                }
+        }
+
+        public void shiftDown(int index) {
+                while (2 * index + 1 < size) {
+                    int left = 2 * index + 1;
+                    int right = 2 * index + 2;
+                    int min = left;
+
+                    if (right < size && buffer[right] < buffer[left]){
+                        min = right;
+                    }
+
+                    if (buffer[index] > buffer[min]) {
+                        int t = buffer[index];
+                        buffer[index] = buffer[min];
+                        buffer[min] = t;
+                        index = min;
+                    } else {
+                        break;
+                    }
+                }
+        }
+
+        public void insert(int val){
+                buffer[size++] = val;
+                shiftUp(size - 1);
+        }
+
+        public int extractMin() {
+                int res = buffer[0];
+                buffer[0] = buffer[--size];
+                if (size > 0) {
+                    shiftDown(0);
+                }
+                return res;
+        }
+    }
+
+    public static void heapsort(int[] arr) {
+        Heap h = new Heap(arr);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = h.extractMin();
+        }
     }
 
     static void main() {
-        for (int i = 1; i <= 2; i++) {
-            for (int j = 1; j <=2; j++) {
-                System.out.println(i + " + " + j + " = " + sum(i, j));
-            }
+        int[] arr = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            arr[i] = rand.nextInt(300);
         }
+        System.out.println(Arrays.toString(arr));
     }
 }
--- Task_1_1_1/src/test/java/ru/nsu/kruzhaev/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/kruzhaev/MainTest.java
@@ -1,12 +1,66 @@
 package ru.nsu.kruzhaev;
 
 import org.junit.jupiter.api.Test;
-
+import java.util.Arrays;
+import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
-    void SumTest() {
-        assertEquals(3, Main.sum(2, 1));
+    void testHeapsort1() {
+        int[] arr = new int[] {};
+        int[] arr_cp = arr.clone();
+        Main.heapsort(arr);
+        Arrays.sort(arr_cp);
+        assertArrayEquals(arr_cp, arr);
+    }
+
+    @Test
+    void testHeapsort2() {
+        int[] arr = new int[] {1};
+        int[] arr_cp = arr.clone();
+        Main.heapsort(arr);
+        Arrays.sort(arr_cp);
+        assertArrayEquals(arr_cp, arr);
+    }
+
+    @Test
+    void testHeapsort3() {
+        int[] arr = new int[] {1, 2, 3, 4};
+        int[] arr_cp = arr.clone();
+        Main.heapsort(arr);
+        Arrays.sort(arr_cp);
+        assertArrayEquals(arr_cp, arr);
+    }
+
+    @Test
+    void testHeapsort4() {
+        int[] arr = new int[] {4, 3, 2, 1};
+        int[] arr_cp = arr.clone();
+        Main.heapsort(arr);
+        Arrays.sort(arr_cp);
+        assertArrayEquals(arr_cp, arr);
+    }
+
+    @Test
+    void testHeapsort5() {
+        int[] arr = new int[] {5, 6, 3, 4, 2, 7, 8, 3, 1, 6};
+        int[] arr_cp = arr.clone();
+        Main.heapsort(arr);
+        Arrays.sort(arr_cp);
+        assertArrayEquals(arr_cp, arr);
+    }
+
+    @Test
+    void testHeapsort6() {
+        int[] arr = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            arr[i] = rand.nextInt(300);
+        }
+        int[] arr_cp = arr.clone();
+        Main.heapsort(arr);
+        Arrays.sort(arr_cp);
+        assertArrayEquals(arr_cp, arr);
     }
 }
