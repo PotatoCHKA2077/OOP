b2b014bea8d0486f7ae7dcde8897d2d35315954e
PotatoCHKA2077 <PotatoCHKA2077@users.noreply.github.com>
2026-09-10 11:51:58 +0000

    Deploying to gh-pages from @ PotatoCHKA2077/Test_OOP@672d03e1c81a149df406991531cf1d0a546f35c6 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.kruzhaev","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.kruzhaev","l":"Main.Heap"},{"p":"ru.nsu.kruzhaev","l":"Main"}];updateSearchResults();
